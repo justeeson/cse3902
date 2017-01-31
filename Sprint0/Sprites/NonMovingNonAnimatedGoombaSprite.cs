6af12f7daf748ed0f8d9649c599bf5ae92f05a39
Sebastin Justeeson <justeeson@hotmail.com>
2017-01-31 12:47:37 -0500

    Changed mario sprite transparency. Added in a new sprite class for goomba.

---
+++ Sprint0/Sprites/NonMovingNonAnimatedGoombaSprite.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Sprint0.Interfaces;
+
+namespace Sprint0.Sprites
+{
+    class NonMovingNonAnimatedGoombaSprite : ISprite
+    {
+        // Modify from http://rbwhitaker.wikidot.com/monogame-texture-atlases-2
+        public Texture2D Texture { get; set; }
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+        private int currentFrame;
+        private int totalFrames;
+        private int timeDelay = 5;
+
+        public NonMovingNonAnimatedGoombaSprite(Texture2D texture, int rows, int columns)
+        {
+            Texture = texture;
+            Rows = rows;
+            Columns = columns;
+            currentFrame = 0;
+            totalFrames = Rows * Columns;
+        }
+
+        public void Update()
+        {   
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 location)
+        {
+            int width = Texture.Width / Columns;
+            int height = Texture.Height / Rows;
+            int row = (int)((float)currentFrame / (float)Columns);
+            int column = currentFrame % Columns;
+
+            Rectangle sourceRectangle = new Rectangle(width * column, height * row, width, height);
+            Rectangle destinationRectangle = new Rectangle((int)location.X, (int)location.Y, width, height);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
+            spriteBatch.End();
+        }
+    }
+}
