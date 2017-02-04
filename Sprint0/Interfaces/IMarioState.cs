5d5874144cf58147fad3dc53122fded63039275a
Sebastin Justeeson <justeeson@hotmail.com>
2017-02-04 18:08:29 -0500

    Removed all unnecessary classes. Added a state class for mario and restructured how the functions in the sprite work. Also, commented out the controller code in game1.cs for now.

---
+++ Sprint0/Interfaces/IMarioState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Sprint0.Interfaces;
+
+namespace Sprint0.Interfaces
+{
+    interface IMarioState
+    {
+        void Update();
+        void Grow();
+        void ChangeDirection();
+        void Draw(SpriteBatch spriteBatch, Vector2 location);
+
+    }
+}
---
+++ Sprint0/States/StandingRightSmallMarioState.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Sprint0.Interfaces;
+
+namespace Sprint0.Sprites
+{
+    class StandingRightSmallMarioState : IMarioState
+    {
+        private Mario mario;
+
+        public StandingRightSmallMarioState(Mario mario)
+        {
+            this.mario = mario;
+        }
+
+        public void ChangeDirection()
+        {
+            
+        }
+        
+        public void Grow()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 location)
+        {
+            int width = mario.Texture.Width / mario.Columns;
+            int height = mario.Texture.Height / mario.Rows;
+            int row = (int)((float)mario.currentFrame / (float)mario.Columns);
+            int column = mario.currentFrame % mario.Columns;
+
+            Rectangle sourceRectangle = new Rectangle(width * column, height * row, width, height);
+            Rectangle destinationRectangle = new Rectangle((int)location.X, (int)location.Y, width, height);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(mario.Texture, destinationRectangle, sourceRectangle, Color.White);
+            spriteBatch.End();
+        }
+
+
+    }
+}
