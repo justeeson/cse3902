6f0959b1babeb03df58a704240c511d7db19c003
son le <tieulyphidep1000@gmail.com>
2017-02-04 20:03:17 -0500

    Add object classes, interfaces, and sprites

---
+++ Sprint0/Interfaces/IMarioState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Sprint0.Interfaces
+{
+    interface IMarioState
+    {   void Update(GameTime gameTime);
+        void Draw(SpriteBatch spriteBatch, Vector2 location, Color color);
+        void Jump();
+        void Fall();
+        void Left();
+        void Crouch();
+        void Idle();
+        void Land();
+        void BigMario();
+        void SmallMario();
+        void FireMario();
+        void FireAbilityMario();
+        void DeadMario();
+    }
+}
