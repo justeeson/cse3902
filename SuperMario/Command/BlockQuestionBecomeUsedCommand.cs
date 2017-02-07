45066c59ae288eb10101039982cdd76073ac3142
Son Le <tieulyphidep1000@gmail.com>
2017-02-07 01:45:11 -0500

    Revert "Finished all the commaned files, the block object class need to be implemented"
    This reverts commit 2969e8ce1aeb43f702e4c0d8a0e04c7ecbed32be.

--- SuperMario/Command/BlockQuestionBecomeUsedCommand.cs
+++ SuperMario/Command/BlockQuestionBecomeUsedCommand.cs
@@ -9,18 +9,7 @@ using SuperMario.Sprites;
 
 namespace SuperMario.Command
 {
-    class BlockQuestionBecomeUsedCommand : ICommand
+    class BlockQuestionBecomeUsedCommand
     {
-        private Game1 myGame;
-
-        public BlockQuestionBecomeUsedCommand(Game1 game)
-        {
-            myGame = game;
-        }
-        public void Execute()
-        {
-            // Question Block object file does not exist
-            //myGame.QuestionBlock.questionToUsed();
-        }
     }
 }
