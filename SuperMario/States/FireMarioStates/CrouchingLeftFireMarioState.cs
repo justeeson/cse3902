25cbcd4fb4e631a177f2b03cd215d376b7426a51
Dellon Collins <collins.dellon@gmail.com>
2017-02-07 23:35:51 -0500

    Fire Mario States

---
+++ SuperMario/States/FireMarioStates/CrouchingLeftFireMarioState.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SuperMario.MarioClass;
+using SuperMario.Interfaces;
+
+namespace SuperMario.Sprites
+{
+    public class CrouchingLeftFireMarioState : IMarioState
+    {
+        private Mario mario;
+        private int currentFrame;
+
+        public CrouchingLeftFireMarioState(Mario mario)
+        {
+            this.mario = mario;
+            currentFrame = 1;
+        }
+
+        public void Update()
+        {
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 location)
+        {
+            int width = mario.Texture.Width / mario.Columns;
+            int height = mario.Texture.Height / mario.Rows;
+            int row = (int)((float)currentFrame / (float)mario.Columns);
+            int column = currentFrame % mario.Columns;
+
+            Rectangle sourceRectangle = new Rectangle(width * column, height * row, width, height);
+            Rectangle destinationRectangle = new Rectangle((int)location.X, 350, width, height);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(mario.Texture, destinationRectangle, sourceRectangle, Color.White);
+            spriteBatch.End();
+        }
+
+
+    }
+}
