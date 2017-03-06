c8e40e4a3ea8919575a4cf54469845009dc1014a
Apple <Apple@DESKTOP-N21BRV0>
2017-03-06 15:21:05 -0500

    Revert "Conflicts"
    This reverts commit 38e9710cab6ea5d5f777fdd0ccb729f4c7a585d7.

---
+++ SuperMario/Command/MarioLookUpCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperMario.Interfaces;
+
+namespace SuperMario.Command
+{
+    public class MarioLookUpCommand : ICommand
+    {
+        private Game1 myGame;
+
+        public MarioLookUpCommand(Game1 game)
+        {
+            myGame = game;
+        }
+
+        public void Execute()
+        {
+            myGame.Mario.LookUp();
+        }
+    }
+}
