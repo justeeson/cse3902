47ef76ee6a4de7bcae23c292c47fb719d54ec4ee
Apple <Apple@DESKTOP-N21BRV0>
2017-03-06 15:21:20 -0500

    Revert "sync"
    This reverts commit a4eed842757345bebfad5c58414c7133e346844e.

--- SuperMario/Game Object Classes/Obstacle/BreakableCurlyBrick .cs
+++ SuperMario/Game Object Classes/Obstacle/BreakableCurlyBrick .cs
@@ -9,20 +9,23 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace SuperMario
 {
-    public class BreakableCurlyBrick : IBlock
+   public class BreakableCurlyBrick : IBlock
     {
         public ISprite Sprite { get; set; }
-        public Game1 MyGame { get; set; }
+        public Game1 myGame { get; set; }
         public Rectangle Area { get; set; }
-        public BreakableCurlyBrick(Game1 game)
+        public Vector2 location { get; set; }
+
+        public BreakableCurlyBrick(Game1 game, Vector2 location)
         {
-            MyGame = game;
+            myGame = game;
             Sprite = SpriteFactory.CreateBreakableCurlyBrick();
-            MyGame.sprite = Sprite;
+            myGame.sprite = Sprite;
+            this.location = location;
         }
+
         public void BrickToDisappear()
         {
-            new SolidBrick(MyGame);
         }
         public void HiddenToUsed()
         {
@@ -30,13 +33,14 @@ namespace SuperMario
         public void BecomeUsed()
         {
         }
+
         public void Update(GameTime gameTime)
         {
             Sprite.Update(gameTime);
         }
         public void Draw(SpriteBatch spriteBatch, Vector2 location)
         {
-            location = new Vector2(this.Area.X, this.Area.Y);
+            location = new Vector2(this.location.X, this.location.Y);
             Sprite.Draw(spriteBatch, location);
         }
     }
