2d2c364f36bdc71348f5bc28b9354f4471c69e3e
Apple <Apple@DESKTOP-N21BRV0>
2017-03-06 15:21:12 -0500

    Revert "Solving conflicts"
    This reverts commit a4176eb6abef772b9e81daa9bb6a5df3951ab006.

---
+++ SuperMario/Command/BlockHiddenBlockUsedCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperMario.Interfaces;
+
+namespace SuperMario.Command
+{
+    public class BlockHiddenBlockUsedCommand : ICommand
+    {
+        private Game1 myGame;
+
+        public BlockHiddenBlockUsedCommand(Game1 game)
+        {
+            myGame = game;
+        }
+        public void Execute()
+        {
+           // myGame.Block.HiddenToUsed();
+        }
+    }
+}
---
+++ SuperMario/Command/BlockQuestionBecomeUsedCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperMario.Interfaces;
+
+namespace SuperMario.Command
+{
+    public class BlockQuestionBecomeUsedCommand : ICommand
+    {
+        private Game1 myGame;
+
+        public BlockQuestionBecomeUsedCommand(Game1 game)
+        {
+            myGame = game;
+        }
+        public void Execute()
+        {
+            //myGame.Block.BecomeUsed();
+        }
+    }
+}
