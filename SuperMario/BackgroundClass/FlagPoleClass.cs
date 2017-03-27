9118da82c6ceeaf7e307404b55ede59695ac7ac0
Son Le <tieulyphidep1000@gmail.com>
2017-03-27 18:21:47 -0400

    update poles and castle

---
+++ SuperMario/BackgroundClass/FlagPoleClass.cs
@@ -0,0 +1,37 @@
+﻿using SuperMario.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace SuperMario
+{
+    public class FlagPole : IBackground
+    {
+        public ISprite Sprite { get; set; }
+        public Game1 MyGame { get; set; }
+        public Vector2 Location { get; set; }
+        public Rectangle Area { get; set; }
+        public FlagPole(Game1 game, Vector2 location)
+        {
+            this.MyGame = game;
+            this.Sprite = SpriteFactory.CreateBackground();
+            this.Location = location;
+        }
+
+        public void Used()
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            Sprite.Update(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 location)
+        {
+            Sprite.Draw(spriteBatch, this.Location);
+        }
+
+    }
+}
