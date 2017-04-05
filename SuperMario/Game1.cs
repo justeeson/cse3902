45613eb5817fa516f7f23b0bb481a522607f3f52
Sebastin Justeeson <justeeson@hotmail.com>
2017-04-05 18:53:52 -0400

    Pause by pressing the 'P' button.

--- SuperMario/Game1.cs
+++ SuperMario/Game1.cs
@@ -19,11 +19,15 @@ namespace SuperMario
         public SpriteBatch SpriteBatch;
         public Texture2D Texture { get; set; }
         private Texture2D background;
+        public Boolean IsPaused;
         private Rectangle mainFrame;
         public GameTime GameTime;
         public SpriteFactory SpriteFactory;
         public Camera CameraPointer;
         private Song backgroundMusic;
+        private Song pauseSoundEffect;
+        private KeyboardState newKeyboardState;
+        private KeyboardState oldKeyboardState;
         private static Game1 instance;
         public WorldManager World;
         public Vector2 Location { get; set; }
@@ -60,7 +64,10 @@ namespace SuperMario
 
         protected override void Initialize()
         {
+            newKeyboardState = new KeyboardState();
+            oldKeyboardState = new KeyboardState();
             ValidKeysList = ValidKeys.Instance.ArrayOfKeys();
+            IsPaused = false;
             CameraPointer = new Camera();
             base.Initialize();
         }
@@ -68,9 +75,11 @@ namespace SuperMario
         protected override void LoadContent()
         {
             SpriteBatch = new SpriteBatch(GraphicsDevice);
+            
             Texture = Content.Load<Texture2D>("MarioSheet");
             background = Content.Load<Texture2D>("background3");
             backgroundMusic = Content.Load<Song>("backgroundMusic");
+            pauseSoundEffect = Content.Load<Song>("pauseSoundEffect");
             //MediaPlayer.IsRepeating = true;
             // MediaPlayer.Play(backgroundMusic);
             mainFrame = new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height);
@@ -93,7 +102,6 @@ namespace SuperMario
             KeyboardController.RegisterCommand(Keys.R, new ResetCommand(this));
             KeyboardController.RegisterCommand(Keys.X, new MarioRunCommand(this));
             KeyboardController.RegisterCommand(Keys.N, new MarioGoToUndergroundCommand(this));
-
 
             GamepadController = new GamepadController();
             GamepadController.RegisterCommand(Buttons.LeftThumbstickLeft, new MarioLookLeftCommand(this));
@@ -120,14 +128,24 @@ namespace SuperMario
         }
         protected override void Update(GameTime GameTime)
         {
-            this.GameTime = GameTime;
-            KeyboardController.Update(GameTime);
-            GamepadController.Update(GameTime);
-            CameraPointer.UpdateX(Mario.LocationX);
-            MarioSprite.Update(GameTime);
-            World.Update(GameTime);
-            Collision_Detection_and_Responses.CollisionHandling.Update(World.Level, this);
-            base.Update(GameTime);           
+            newKeyboardState = Keyboard.GetState();
+            if (newKeyboardState.IsKeyDown(Keys.P) && oldKeyboardState.IsKeyUp(Keys.P))
+            {              
+                IsPaused = !IsPaused;
+                MediaPlayer.Play(pauseSoundEffect);
+            }
+            oldKeyboardState = newKeyboardState;
+            if (!IsPaused)
+            {
+                this.GameTime = GameTime;
+                KeyboardController.Update(GameTime);
+                GamepadController.Update(GameTime);
+                CameraPointer.UpdateX(Mario.LocationX);
+                MarioSprite.Update(GameTime);
+                World.Update(GameTime);
+                Collision_Detection_and_Responses.CollisionHandling.Update(World.Level, this);
+                base.Update(GameTime);
+            }
         }
 
         protected override void Draw(GameTime GameTime)
